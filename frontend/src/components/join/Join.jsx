45c6d82d374209188904e124f31e7dcd3dfd5535
Johann4904 <johdokdci@web.de>
2024-04-01 12:12:09 +0200

    first commit

--- frontend/src/components/join/Join.jsx
+++ frontend/src/components/join/Join.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Join() {
   return (
-    <div className='join-container' style={{ border:'1px solid red', height: '400px'}}>Join
+    <div className='join-container' style={{ border:'1px solid red', height: '300px'}}>Join
         <button>button1</button>
         <button>button2</button>
         <button>button3</button>
