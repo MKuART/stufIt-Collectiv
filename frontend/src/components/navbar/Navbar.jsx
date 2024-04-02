80b7e8534edd929982d6138bc9cbbca5338466e0
Michél Kühne <3in6indun9@googlemail.com>
2024-04-02 13:59:47 +0200

    logo first itteration 6-div-version

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import './navbar.css';
 import { NavLink, Outlet, useNavigate, Routes, Route } from 'react-router-dom';
 import Join from '../join/Join.jsx';
 import Login from '../login/Login.jsx';
@@ -23,6 +24,17 @@ function Navbar() {
     return (
         <>
             <nav style={{ border: '1px solid red', height: '90px', display:'flex' }}>
+                <div className='logo-container'>
+                    <div className='cards-container'>
+                        <div className='card a-card'></div>
+                        <div className='card b-card'></div>
+                        <div className='card c-card'>0930934827349</div>
+                        <div className='card d-card'></div>
+                        <div className='card f-card'></div>
+                        <div className='card g-card'></div>
+                    </div>
+                    <h2 className='logo-text'>Stuffit</h2>
+                </div>
                 <button onClick={userId ? handleLogout : handleLogin}>
                     {userId ? 'Logout' : 'Login'}
                 </button>
