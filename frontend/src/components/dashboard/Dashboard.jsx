de510a8c57d68d6eb54146be85a22df1e6cd0180
TwinMine <michaelrolo11@gmail.com>
2024-04-16 15:26:39 +0200

    fix merge problems

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,24 +1,24 @@
 import { useContext, useEffect, useRef, useState } from "react";
 import Legend from "./legend/Legend.jsx";
 import UserData from "../Context/UserData.jsx";
-import { useNavigate } from "react-router-dom";
 
 const URIAccount = "http://localhost:1305/account";
 const URICategory = "http://localhost:1305/category";
 const URIExpenses = "http://localhost:1305/expenses";
 
 function Dashboard() {
-  const [ account, setAccount ] = useState(null);
-  const [ categories, setCategories ] = useState([]);
+  const [account, setAccount] = useState(null);
+  const [categories, setCategories] = useState([]);
   const { userData, setUserData } = useContext(UserData);
-  const [ deleteMode, setDeleteMode ] = useState(false);
-  const [ newCategoryName, setNewCategoryName ] = useState("");
-  const [ newCategoryBudget, setNewCategoryBudget ] = useState(0);
-  const [ creatingCategory, setCreatingCategory ] = useState(false); 
-  const [ expenses, setExpenses ] = useState([]);
-  const [ errorMessage, setErrorMessage ] = useState("");
-  const [ selectedCategoryId, setSelectedCategoryId ] = useState(null);
-
+  const [deleteMode, setDeleteMode] = useState(false);
+  const [newCategoryName, setNewCategoryName] = useState("");
+  const [newCategoryBudget, setNewCategoryBudget] = useState(0);
+  const [creatingCategory, setCreatingCategory] = useState(false);
+  const [expenses, setExpenses] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [selectedCategoryId, setSelectedCategoryId] = useState(null);
+  const newName = useRef(null);
+  const newBudget = useRef(null);
   async function fetchExpenses(categoryId) {
     try {
       const response = await fetch(URIExpenses, {
@@ -26,34 +26,20 @@ function Dashboard() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ category: categoryId }), 
+        body: JSON.stringify({ category: categoryId }),
         credentials: "include",
       });
       if (!response.ok) {
-        throw new Error(`Error fetching expenses: ${response.status}`); 
+        throw new Error(`Error fetching expenses: ${response.status}`);
       }
-      const data = await response.json(); 
-      setExpenses(data); 
+      const data = await response.json();
+      setExpenses(data);
       console.log(data);
     } catch (error) {
-      console.error("Error fetching expenses data:", error); 
+      console.error("Error fetching expenses data:", error);
       setErrorMessage("fetch failed", userData);
     }
   }
-
-
-
-function Dashboard() {
-  const Navigate = useNavigate();
-  const [account, setAccount] = useState(null);
-  const [categories, setCategories] = useState([]);
-  const { userData, setUserData } = useContext(UserData);
-  const [deleteMode, setDeleteMode] = useState(false);
-  const [newCategoryName, setNewCategoryName] = useState("");
-  const [newCategoryBudget, setNewCategoryBudget] = useState(0);
-  const [creatingCategory, setCreatingCategory] = useState(false); // Zustand für das Erstellen einer Kategorie
-  const newName = useRef(null);
-  const newBudget = useRef(null);
 
   async function fetchCategories() {
     try {
@@ -77,7 +63,6 @@ function Dashboard() {
       console.log(`Error: ${error}`);
     }
   }
-
 
   async function fetchAccount() {
     try {
@@ -126,60 +111,58 @@ function Dashboard() {
         }),
       });
       const data = await response.json();
-        await fetchAccount();
-        await fetchCategories();
-       setNewCategoryName("");
-       setNewCategoryBudget(0);
-      
+      await fetchAccount();
+      await fetchCategories();
+      setNewCategoryName("");
+      setNewCategoryBudget(0);
+
       if (!response.ok) {
         console.error("Error creating category:", response.statusText);
-      } else { 
-        setCategories((prevCategories) => [
-          ...prevCategories,
-          data,
-        ]);
+      } else {
+        setCategories((prevCategories) => [...prevCategories, data]);
       }
     } catch (error) {
       console.log(`Error: ${error}`);
     }
   };
-  
 
   const deleteOneCategoryFromAccount = async (newCategory) => {
     try {
-      console.log("Userdata Id:", userData._id,  "Category-Id: ", newCategory);
-      
+      console.log("Userdata Id:", userData._id, "Category-Id: ", newCategory);
+
       const response = await fetch(`${URIAccount}/update`, {
-        method: "PATCH", 
+        method: "PATCH",
         headers: {
           "Content-Type": "application/json",
           Authorization: userData.role,
         },
         credentials: "include",
         body: JSON.stringify({
-          userId: userData._id, 
-          categoryIdToRemove: newCategory 
+          userId: userData._id,
+          categoryIdToRemove: newCategory,
         }),
       });
       const data = await response.json();
       console.log("Data: ", data);
-      
+
       if (!response.ok) {
         console.error("Error deleting category:", response.statusText);
       } else {
         console.log("Category deleted!", response.statusText);
 
-        setUserData({ ...userData, category: userData.category.filter(catId => catId !== newCategory) });
-
+        setUserData({
+          ...userData,
+          category: userData.category.filter((catId) => catId !== newCategory),
+        });
       }
-    } catch(error) {
-      console.error("Error: ", error)
+    } catch (error) {
+      console.error("Error: ", error);
     }
-  }
-  
+  };
+
   const deleteCategory = async (categoryId) => {
     try {
-      const newCategoryId = categoryId
+      const newCategoryId = categoryId;
       const response = await fetch(`${URICategory}/hard-delete`, {
         method: "DELETE",
         headers: {
@@ -196,9 +179,9 @@ function Dashboard() {
         console.error("Error deleting category:", response.statusText);
       } else {
         console.log("Category gelöscht!", response.statusText);
-        
-        fetchCategories()
-        deleteOneCategoryFromAccount(newCategoryId)
+
+        fetchCategories();
+        deleteOneCategoryFromAccount(newCategoryId);
       }
     } catch (error) {
       console.log(`Error: ${error}`);
@@ -210,8 +193,8 @@ function Dashboard() {
   };
 
   const handleCategoryClick = (categoryId) => {
-    setSelectedCategoryId(categoryId); 
-    fetchExpenses(categoryId); 
+    setSelectedCategoryId(categoryId);
+    fetchExpenses(categoryId);
   };
 
   useEffect(() => {
@@ -219,113 +202,111 @@ function Dashboard() {
   }, [selectedCategoryId]);
 
   useEffect(() => {
+    fetchCategories();
+    fetchAccount();
+  }, []);
 
-  fetchCategories();
-  fetchAccount();
-}, []);
+  useEffect(() => {
+    fetchExpenses(selectedCategoryId);
+  }, [selectedCategoryId]);
 
-useEffect(() => {
-  fetchExpenses(selectedCategoryId);
-}, [selectedCategoryId]);
-
-
-  const categoryBudgets = categories.map(category => category.limitedBudget);
-  const sumCategoryBudgets = categoryBudgets.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-  const totalRest = userData.budget - sumCategoryBudgets
+  const categoryBudgets = categories.map((category) => category.limitedBudget);
+  const sumCategoryBudgets = categoryBudgets.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  const totalRest = userData.budget - sumCategoryBudgets;
   const getColor = (percent) => {
-    if (percent > 50 ) {
-      return 'greenyellow';
-    } else if ( percent > 30 ) {
-      return 'yellow';
+    if (percent > 50) {
+      return "greenyellow";
+    } else if (percent > 30) {
+      return "yellow";
     } else {
-      return 'red';
+      return "red";
     }
-  } 
-
-
-
+  };
 
   console.log(categories);
-  
+
   return (
     <div
       className="dashboard-container"
       style={{ height: "600px", marginTop: "150px", position: "relative" }}
     >
-      ({expenses.cost})
-      ({})
-      {totalRest}€ want to stuff
+      ({expenses.cost}) ({}){totalRest}€ want to stuff
       <div className="name-container" style={{}}>
         {<Legend />}
       </div>
       <div className="cotegory-container" style={{}}>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
 
-      {errorMessage && <div className="error-message">{errorMessage}</div>}
+        {categories && userData ? (
+          categories.map((category) => {
+            const categoryExpenses = expenses.filter((expense) =>
+              expense.category.includes(category._id)
+            );
+            const totalExpenseCost = categoryExpenses.reduce(
+              (acc, curr) => acc + curr.cost,
+              0
+            );
+            const currentPercent =
+              ((totalExpenseCost / category.limitedBudget) * 100 - 100) * -1;
 
+            return (
+              <div key={category._id} className="div-container">
+                <div onClick={() => handleCategoryClick(category._id)}>
+                  {category.name}
+                </div>
+                {selectedCategoryId === category._id && (
+                  <ul>
+                    {categoryExpenses.map((expense) => (
+                      <li key={expense._id}>
+                        {expense.description}: {expense.cost}€
+                      </li>
+                    ))}
+                  </ul>
+                )}
 
-
-      {categories && userData ? (
-        categories.map((category) => {
-          const categoryExpenses = expenses.filter(expense => expense.category.includes(category._id));
-          const totalExpenseCost = categoryExpenses.reduce((acc, curr) => acc + curr.cost, 0);
-          const currentPercent = (((totalExpenseCost / category.limitedBudget) * 100) - 100) * -1;
-          
-
-          return (
-            <div key={category._id} className="div-container">
-              <div onClick={() => handleCategoryClick(category._id)}>
-                {category.name}
-              </div>
-              {selectedCategoryId === category._id && (
-                <ul>
-                  {categoryExpenses.map((expense) => (
-                    <li key={expense._id}>
-                      {expense.description}: {expense.cost}€
-                    </li>
-                  ))}
-                </ul>
-              )}
-             
-              <div 
-                className="procent-container" 
-                style={{ 
-                  "--current-percent": `${currentPercent}%`,
-                  backgroundImage: `conic-gradient(
+                <div
+                  className="procent-container"
+                  style={{
+                    "--current-percent": `${currentPercent}%`,
+                    backgroundImage: `conic-gradient(
                     ${getColor(currentPercent)} var(--current-percent), 
                     #545F66 var(--current-percent),
                     #545F66 100%)`,
-                  zIndex: '-1'
-                }}
-              />
-            </div>
-            
-          );
-        })
-      ) : (
-          <p>Loading...</p>)}
+                    zIndex: "-1",
+                  }}
+                />
+              </div>
+            );
+          })
+        ) : (
+          <p>Loading...</p>
+        )}
 
-  {categories && userData ? (
-    categories.map((category) => (
-      <div
-        className="div-container"
-        key={category?._id}
-        style={{ textAlign: "center" }}
-      >
-        {category && category?.name}
-        {deleteMode && (
-          <div
-            className="deleteIcon"
-            onClick={() => handleDeleteClick(category?._id)}
-          >
-            X
-          </div>
+        {categories && userData ? (
+          categories.map((category) => (
+            <div
+              className="div-container"
+              key={category?._id}
+              style={{ textAlign: "center" }}
+            >
+              {category && category?.name}
+              {deleteMode && (
+                <div
+                  className="deleteIcon"
+                  onClick={() => handleDeleteClick(category?._id)}
+                >
+                  X
+                </div>
+              )}
+            </div>
+          ))
+        ) : (
+          <p>Loading...</p>
         )}
       </div>
-    ))
-  ) : (
-    <p>Loading...</p>
-  )}
-</div>
       <div
         className="join-container"
         style={{
@@ -340,7 +321,6 @@ useEffect(() => {
           left: "2vh",
         }}
       >
-
         {!creatingCategory && (
           <button className="btn" onClick={() => setCreatingCategory(true)}>
             Kategorie erstellen
